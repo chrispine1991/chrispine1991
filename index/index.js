c0c5dd3bcb002c64921b1c3d98fbf91da4c3506a
Chris Pine Public GIT <104289780+chrispine1991@users.noreply.github.com>
2022-08-17 17:22:04 -0700

    Update index.js

--- index/index.js
+++ index/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (red,res) => {
-
+  res.send('Hello World!');
 });
+
+app.listen(3000, () => console.log('Listening On Port 3000...'));
